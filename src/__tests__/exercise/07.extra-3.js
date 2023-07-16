fcd2adbc76e7274974a98122e3148dc3f6828402
Reysmer Valle <reysmerwvr@gmail.com>
2023-07-16 18:11:11 +0200

    complete mocking browser APIs and modules

--- src/__tests__/exercise/07.extra-3.js
+++ src/__tests__/exercise/07.extra-3.js
@@ -2,19 +2,25 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen} from 'test/test-utils.js'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
-  )
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  render(<EasyButton>Easy</EasyButton>)
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
 
